b489994d1a898c80517b4ff123c4576648d1923d
imyulm <imyurimm@gmail.com>
2022-11-23 22:50:10 +0900

    20208

--- backtracking/20208.py
+++ backtracking/20208.py
@@ -5,3 +5,39 @@
 import sys
 
 N, M, H = map(int, sys.stdin.readline().split())
+
+graph = []
+for _ in range(N):
+    graph.append(list(map(int, sys.stdin.readline().split())))
+
+milks = []
+hx = 0
+hy = 0
+answer = 0
+
+for i in range(N):
+    for j in range(N):
+        if graph[i][j] == 2:
+            milks.append([i, j])
+        elif graph[i][j] == 1:
+            hx = i
+            hy = j
+
+
+def dfs(x, y, health, milk_cnt):
+    global answer
+
+    if abs(x - hx) + abs(y - hy) <= health:
+        answer = max(answer, milk_cnt)
+
+    for mx, my in milks:
+        if graph[mx][my] == 2:
+            d = abs(mx-x) + abs(my-y)
+            if health - d >= 0:
+                graph[mx][my] = 0
+                dfs(mx, my, health-d+H, milk_cnt+1)
+                graph[mx][my] = 2
+
+
+dfs(hx, hy, M, 0)
+print(answer)
