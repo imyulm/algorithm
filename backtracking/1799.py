a4bdbed2231826bf4eba8308b7374a0763d54b30
imyulm <imyurimm@gmail.com>
2022-12-10 00:02:26 +0900

    1799

--- backtracking/1799.py
+++ backtracking/1799.py
@@ -3,16 +3,20 @@
 import sys
 N = int(input())
 
-# 오위 -> 왼아
+# 대각선 종류 2가지
+# 1. 오위 -> 왼아
+visited_rightTop = [False]*(N*2-1)
+# 2. 왼위 -> 오아
+visited_leftTop = [False]*(N*2-1)
+
 # 비숍이 있을 수 있는 칸 모으기
-rightTop = []
-visited_rightTop = [[False]*N for _ in range(N)]
-rightTop_cnt = 0
-# 왼위 -> 오아
-# 비숍이 있을 수 있는 칸 모으기
-leftTop = []
-visited_leftTop = [[False]*N for _ in range(N)]
-leftTop_cnt = 0
+# 1. (0,0), (0,2), (2,0), (1,1) ...
+bishop1 = []
+bishop1_cnt = 0
+
+# 2. (1,0), (0,1), (0,3), (1,2), (2,1), (3,0) ...
+bishop2 = []
+bishop2_cnt = 0
 
 board = []
 
@@ -23,18 +27,18 @@ for i in range(N):
     for j in range(N):
         if board[i][j] != 1: continue
         if i % 2 == 0 and j % 2 == 0:
-            rightTop.append([i, j])
+            bishop1.append([i, j])
         else:
-            leftTop.append([i, j])
+            bishop2.append([i, j])
 
 def dfs(bishop, idx, cnt):
-    global rightTop_cnt, leftTop_cnt
+    global bishop1_cnt, bishop2_cnt
     if idx == len(bishop):
         x, y = bishop[0]
         if x % 2 == 0 and y % 2 == 0:
-            rightTop_cnt = max(cnt, rightTop_cnt)
+            bishop1_cnt = max(cnt, bishop1_cnt)
         else:
-            leftTop_cnt = max(cnt, leftTop_cnt)
+            bishop2_cnt = max(cnt, bishop2_cnt)
         return
 
     x, y = bishop[idx]
@@ -44,13 +48,15 @@ def dfs(bishop, idx, cnt):
         visited_rightTop[x+y] = True
         visited_leftTop[x-y+N-1] = True
         dfs(bishop, idx+1, cnt+1)
-        visited_rightTop[x + y] = False
-        visited_leftTop[x - y + N - 1] = False
+        visited_rightTop[x+y] = False
+        visited_leftTop[x-y+N-1] = False
         dfs(bishop, idx+1, cnt+1)
 
 
 # 2개로 쪼개서 2^(5*5) * 2 계산으로 줄임
-if len(rightTop) > 0:
-    dfs(rightTop, 0, 0)
-if len(leftTop) > 0:
-    dfs(leftTop, 0, 0)
+if len(bishop1) > 0:
+    dfs(bishop1, 0, 0)
+if len(bishop2) > 0:
+    dfs(bishop2, 0, 0)
+
+print(bishop1_cnt + bishop2_cnt)
