50ce80ff33ba78c6e89bbebe72d1b58e4ff3fb63
imyulm <imyurimm@gmail.com>
2022-12-13 23:40:55 +0900

    1342

--- backtracking/1342.py
+++ backtracking/1342.py
@@ -4,20 +4,16 @@ S = list(input())
 # 각 문자열 자리 방문 여부
 visited = [False] * len(S)
 
-answer = 0
-answer_list = []
+answer_list = set()
 
 def dfs(s):
-    global answer
     if len(s) == len(S):
         tmp = s[0]
         for i in range(1, len(s)):
             if tmp == s[i]:
                 return
             tmp = s[i]
-        if str(s) not in answer_list:
-            answer += 1
-            answer_list.append(str(s))
+        answer_list.add(str(s))
         return
 
     for i in range(len(S)):
@@ -30,4 +26,4 @@ def dfs(s):
 
 
 dfs([])
-print(answer)
+print(len(answer_list))
