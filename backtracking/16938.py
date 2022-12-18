373d7e4ee0c86aad66f455819c3fd5fe80abbe6b
imyulm <imyurimm@gmail.com>
2022-12-18 22:07:12 +0900

    16938

---
+++ backtracking/16938.py
@@ -0,0 +1,9 @@
+import sys
+
+# N: 문제 개수, L: 문제 최저 난이도, R: 문제 최고 난이도, X: R-L의 최소 차이
+N, L, R, X = map(int, sys.stdin.readline().split())
+
+A_list = list(map(int, sys.stdin.readline().split()))
+
+# 2문제 이상 골라야 함
+
