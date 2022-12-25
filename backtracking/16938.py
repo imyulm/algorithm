4dec4130734fe7ef404c1c19d2e39d09737c9ba5
imyulm <imyurimm@gmail.com>
2022-12-26 00:13:23 +0900

    16938

--- backtracking/16938.py
+++ backtracking/16938.py
@@ -5,12 +5,13 @@ N, L, R, X = map(int, sys.stdin.readline().split())
 
 A_list = list(map(int, sys.stdin.readline().split()))
 
-answer_list = set()
+answer = 0
 
 # 2문제 이상 골라야 함
 # 중복 불가 조합
 def dfs(idx, selected):
     global answer
+    if idx > len(A_list):return
     if len(selected) >= 2:
         sum_A = 0
         tmp_L = 10000000000
@@ -21,16 +22,14 @@ def dfs(idx, selected):
             tmp_R = max(tmp_R, A)
 
         if (L <= sum_A <= R) and (tmp_R - tmp_L >= X):
-            answer_list.add(str(selected))
-        return
+            answer += 1
 
     for i in range(idx, N):
         selected.append(A_list[i])
-        dfs(idx+1, selected)
+        dfs(i+1, selected)
         selected.pop()
-        dfs(idx+1, selected)
 
 
 
 dfs(0, list())
-print(len(answer_list))
+print(answer)
